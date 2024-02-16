cb6fd2e57b4eb4e7753d782ccf7d11144e1861a5
Ian Wise <ixwise0318@gmail.com>
2024-02-16 14:19:08 -0600

    Lab Exercise - Writing Basic Statements

--- JavaFundamentals/src/main/java/com/fundamentals/main/Main.java
+++ JavaFundamentals/src/main/java/com/fundamentals/main/Main.java
@@ -4,12 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         houseExample();
+        basicStatements();
     }
 
     // Single-Line Comment
     /* Multi-Line Comment
-    This is the second line
-    This is the third.
+    This is the Second Line
+    This is the Third Line.
      */
 
     public static void houseExample() {
@@ -20,5 +21,12 @@ public class Main {
         myHouse.roofStyle = "Slate Hip Roof";
         myHouse.totalWindows = 33;
         System.out.println(myHouse.entranceDoorColor);
+    }
+
+    public static void basicStatements() {
+        System.out.println("This is my first sentence.");
+        System.out.println("I am new to Java.");
+        System.out.println("This Java course is cool.");
+        System.out.println("I am learning stuff every day.");
     }
 }
