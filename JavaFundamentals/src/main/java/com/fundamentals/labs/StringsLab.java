eb267851f1ec0583dfe9bf1997fcbeaac6c9916d
Ian Wise <ixwise0318@gmail.com>
2024-02-20 16:45:15 -0600

    Lab Exercise - Using Strings

---
+++ JavaFundamentals/src/main/java/com/fundamentals/labs/StringsLab.java
@@ -0,0 +1,19 @@
+package com.fundamentals.labs;
+
+public class StringsLab {
+    public void helloMyName(String sentence) {
+        System.out.println(sentence.toLowerCase());
+        System.out.println(sentence.charAt(17));
+        System.out.println(sentence.length());
+    }
+
+    public void nurseryRhyme() {
+        String rhyme = "Jack and Jill went up the hill \nto fetch a pail of water.\nJack fell down and broke his crown \nand Jill came tumbling after.";
+        System.out.println(rhyme);
+    }
+    public static void main(String[] args) {
+        StringsLab myStringLab = new StringsLab();
+        myStringLab.helloMyName("Hello my name is Ian Wise.");
+        myStringLab.nurseryRhyme();
+    }
+}
