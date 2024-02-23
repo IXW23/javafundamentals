72c4c41d217f5590e01f344718e95a602bb3d3bc
Ian Wise <ixwise0318@gmail.com>
2024-02-23 17:17:02 -0600

    Reapply "Lab Exercise - Conditional Statements"
    This reverts commit 12eb470cc1080b495136e69ffd53e9847b835831.

--- JavaFundamentals/src/main/java/com/fundamentals/labs/ConditionsLab.java
+++ JavaFundamentals/src/main/java/com/fundamentals/labs/ConditionsLab.java
@@ -1,2 +1,43 @@
-package com.fundamentals.labs;public class ConditionsLab {
+package com.fundamentals.labs;
+
+public class ConditionsLab {
+    public void ifElseStatement(String color1, String color2) {
+        if(color1.equals(color2)) {
+            System.out.println(color1 + " is the same color as " + color2 + ".");
+        } else {
+            System.out.println(color1 + " is not the same color as " + color2 + ".");
+        }
+    }
+
+    public String switchStatement(char grade) {
+        String message;
+        switch(grade) {
+            case 'E':
+                message = "Excellent Grade";
+                break;
+            case 'V':
+                message = "Very Good Grade";
+                break;
+            case 'G':
+                message = "Good Grade";
+                break;
+            case 'A':
+                message = "Average Grade";
+                break;
+            case 'F':
+                message = "Failed Grade";
+                break;
+            default:
+                message = "Not a Valid Grade";
+        }
+        return message;
+    }
+
+    public static void main(String[] args) {
+        ConditionsLab myConditionsLab = new ConditionsLab();
+        myConditionsLab.ifElseStatement("orange", "orange");
+        myConditionsLab.ifElseStatement("orange", "blue");
+        System.out.println(myConditionsLab.switchStatement('E'));
+        System.out.println(myConditionsLab.switchStatement('L'));
+    }
 }
